1b477c9f211b3d1e35a9ddec74dd3846649c465a
Minh Duc Vo <paulthimony@gmail.com>
2026-06-18 13:58:43 -0500

    add changes to pathlib file

--- Pathlib.py
+++ Pathlib.py
@@ -3,4 +3,30 @@
 import os
 from pathlib import Path
 
-print(Path.cwd())
+'''print(Path.cwd())
+for p in Path().iterdir():
+    print(p)'''
+
+'''
+my_dir = path("Directory_1")
+my_file = Path("file_1.txt")
+
+new_file = my_dir / "new_file.txt"  ( my_dir.joinpath(...))
+
+print(my_dir.suffix(name, stem,..))
+print(my_file.exists())
+print(new_file.parent.absoulte())
+'''
+
+#p = Path(__file__).resolve().parent.parent
+dotfiles = Path.home() / "dotfiles"
+
+#for p in dotfiles.rglob("*vscode*", case_sensitive=False):
+    
+p = Path("TempDir")
+#p.mkdir(parents=True) #Create directory (create any parent directory on the way)
+#p.rmdir() #Remove only empty directory
+file = Path("temp_file.txt")
+#file.touch()
+#file.replace("temp_file.txt")
+file.unlink()
